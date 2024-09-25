6ac241481cd7e59dbc59f0ed0096083ee570e096
sukritiguin <sukritiguin.ece2020@nsec.ac.in>
2024-09-25 19:05:38 +0530

    fix(single chat UI) - fix the backgound color of sender and receiver message

--- src/app/message/components/ReceivedMessage.tsx
+++ src/app/message/components/ReceivedMessage.tsx
@@ -6,7 +6,7 @@ export const ReceivedMessage = () => {
   return (
     <div className="flex items-start space-x-2">
       <FaUserCircle className="text-gray-500 w-6 h-6" />
-      <div className="flex flex-col bg-gray-100 px-3 py-2 rounded-lg text-sm text-gray-800">
+      <div className="flex flex-col bg-gray-50 px-3 py-2 rounded-lg text-sm text-gray-800">
         <span className="font-semibold">John Doe</span>
         <span>
           Hi John! I just wanted to follow up on the project proposal.
--- src/app/message/components/SentMessage.tsx
+++ src/app/message/components/SentMessage.tsx
@@ -5,7 +5,7 @@ export const SentMessage = () => {
   const image = "";
   return (
     <div className="flex items-start space-x-2 justify-end">
-      <div className="flex flex-col bg-gray- px-3 py-2 rounded-lg text-sm text-gray-800">
+      <div className="flex flex-col bg-gray-300 px-3 py-2 rounded-lg text-sm text-gray-800">
         <span className="font-semibold">John Doe</span>
         <span>
           Hi John! I just wanted to follow up on the project proposal.
